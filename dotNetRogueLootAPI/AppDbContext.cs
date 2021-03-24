a55b305741cdfa5df839bfe632128bf8073c0950
Julian <Julian-Tekstra@hotmail.com>
2021-03-24 10:45:45 +0100

    Add EntityFrameworkCore

---
+++ dotNetRogueLootAPI/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotNetRogueLootAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetRogueLootAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<WeaponType> WeaponTypes { get; set; }
+        public DbSet<WeaponRarity> WeaponRarities { get; set; }
+    }
+}
